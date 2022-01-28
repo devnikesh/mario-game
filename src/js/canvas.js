d4f19a22d3a2707bb8512f3072abfafd93142bc8
Nikesh <nikesh123u@gmail.com>
2022-01-28 23:15:17 +0545

    Add Objects

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,11 +1,16 @@
+import platform from "../img/platform.png";
+import background from "../img/background.png";
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d"); // What type of context do we want, for now we creating a 2d game
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.width = 1024;
+canvas.height = 576;
 
 //  Gravity
 const gravity = 0.5;
 const speed = 10;
+
+const platformImage = createImage(platform);
+const backgroundImage = createImage(background);
 
 // Player
 
@@ -40,7 +45,7 @@ class Player {
   }
 }
 class Platform {
-  constructor({ x, y }) {
+  constructor({ x, y, image }) {
     this.position = {
       x,
       y,
@@ -49,28 +54,65 @@ class Platform {
       x: 0,
       y: 0,
     };
-    this.width = 200;
-    this.height = 20;
+    this.image = image;
+    this.width = image.width;
+    this.height = image.height;
   }
   // Define a method || Draw the Player, Default value is gonna be applied if any value is not passed
   draw() {
-    c.fillStyle = "blue";
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    c.drawImage(this.image, this.position.x, this.position.y);
   }
-  update() {
-    this.draw();
+}
+
+class GenericObject {
+  constructor({ x, y, image }) {
+    this.position = {
+      x,
+      y,
+    };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.image = image;
+    this.width = image.width;
+    this.height = image.height;
   }
+  // Define a method || Draw the Player, Default value is gonna be applied if any value is not passed
+  draw() {
+    c.drawImage(this.image, this.position.x, this.position.y);
+  }
+}
+const genericObjects = [
+  new GenericObject({
+    x: 0,
+    y: 0,
+    image: backgroundImage,
+  }),
+  new GenericObject({
+    x: 0,
+    y: 0,
+    image: backgroundImage,
+  }),
+];
+
+function createImage(imageSrc) {
+  const image = new Image();
+  image.src = imageSrc;
+  return image;
 }
 
 const player = new Player();
 const platforms = [
   new Platform({
-    x: 200,
-    y: 100,
+    x: 0,
+    y: 470,
+    image: platformImage,
   }),
   new Platform({
-    x: 500,
-    y: 100,
+    x: platformImage.width - 2,
+    y: 470,
+    image: platformImage,
   }),
 ];
 const keys = {
@@ -86,11 +128,15 @@ let scrollOffset = 0;
 
 function animate() {
   requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
-  player.update();
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  genericObjects.forEach((object) => {
+    object.draw();
+  });
   platforms.forEach((platform) => {
     platform.draw();
   });
+  player.update();
 
   if (keys.right.pressed) {
     scrollOffset += 5;
@@ -141,7 +187,7 @@ function animate() {
     }
   });
   if (scrollOffset > 2000) {
-    console.log("You Win!");
+    alert("You Win!");
   }
 }
 
